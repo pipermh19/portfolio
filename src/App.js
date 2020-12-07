b4fea93b6a6d2e2a10fb2cb31aad63961106703f
unknown <piper.1.hayden@gmail.com>
2020-12-07 08:14:15 -0600

    Adjusting routes

--- src/App.js
+++ src/App.js
@@ -62,13 +62,13 @@ class App extends React.Component {
               </Navbar.Collapse>
           </Navbar>
 
-          <Route exact path="/"  render={() => <HomePage title={this.state.home.title}  subTitle={this.state.home.subTitle} text={this.state.home.text}  />} />
-          <Route path="/about" render={() => <AboutPage title={this.state.about.title} />} />
-          <Route path="/contact" render={() => <ContactPage title={this.state.contact.title} />} />
-          <Route path="/work" render={() => <WorkPage title={this.state.work.title} />} />
-          <Route path="/GoCorsicanaPage" render={() => <GoCorsicanaPage/>} />
-          <Route path="/TrackerPage" render={() => <TrackerPage/>} />
-          <Route path="/CatalogPage" render={() => <CatalogPage/>} />
+          <Route exact path="/" component={Home}/>
+          <Route path="/about" component={About}/>
+          <Route path="/contact" component={Contact} />
+          <Route path="/work" component={Work} />
+          <Route path="/GoCorsicanaPage" component={GoCorsicana} />
+          <Route path="/TrackerPage" component={Tracker} />
+          <Route path="/CatalogPage" component={Catalog} />
 
           {/*<Footer />*/}
 
@@ -76,7 +76,14 @@ class App extends React.Component {
       </HashRouter>
     );
   }
-
 }
 
-export default App;
+  const Home = () => <HomePage title={this.state.home.title}  subTitle={this.state.home.subTitle} text={this.state.home.text} />
+  const About = () => <AboutPage title={this.state.about.title} />
+  const Contact = () => <ContactPage title={this.state.contact.title} />
+  const Work = () => <WorkPage title={this.state.work.title} />
+  const GoCorsicana = () => <GoCorsicanaPage/>
+  const Tracker = () => <TrackerPage/>
+  const Catalog = () => <CatalogPage/>
+
+  export default App;
